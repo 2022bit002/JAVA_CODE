b6902ae4238b7a9d236eb489ca2279e94c15363f
2022bit002 <2022bit002@sggs.ac.in>
2024-04-14 09:58:42 +0530

    stack Exception in java

---
+++ stackExcep.java
@@ -0,0 +1,16 @@
+class stackExcept{
+	public static void main(String args[]){
+	try{
+		int num1=234;
+		int num2=0;
+		if(num2==0){
+			throw new excption();
+		}
+		catch (Exception excption){
+			Sytsem.out.println("ExceptionS");
+		}
+	}
+	
+	}
+
+}
