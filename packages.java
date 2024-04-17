f6d0df3ac87510a0e720e6556a6bf1f7800a34f5
2022bit002 <2022bit002@sggs.ac.in>
2024-04-17 10:04:28 +0530

    learning the packages

---
+++ packages.java
@@ -0,0 +1,12 @@
+// creating a package
+
+package pankaj;
+
+public class packages{
+	public static void main(String args[]){
+		System.out.println("you have open the packages that is created manually by pankaj");
+	}
+	public void name(){
+		System.out.println("this is inside the package");
+	}
+}
