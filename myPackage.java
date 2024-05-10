d861d38ab9c7a7a5cc34f542050c246eba236f08
2022bit002 <2022bit002@sggs.ac.in>
2024-05-10 10:35:35 +0530

    learning the packages

---
+++ myPackage.java
@@ -0,0 +1,14 @@
+
+
+import pankaj.packages;
+
+class UsingPackage{
+	
+	public static void main(String args[]){
+		packages p = new packages();
+		System.out.println("in this file we have use the another file using import");
+		p.name();
+		
+	}
+}
+ 
